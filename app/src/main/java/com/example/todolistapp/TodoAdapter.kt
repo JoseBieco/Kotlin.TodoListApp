667832d71262640703bb9112b07c3c3776cb017f
Jose Bieco <jo_bieco@hotmail.com>
2021-10-06 22:33:52 -0300

    TodoAdapter

--- app/src/main/java/com/example/todolistapp/TodoAdapter.kt
+++ app/src/main/java/com/example/todolistapp/TodoAdapter.kt
@@ -1,4 +1,33 @@
 package com.example.todolistapp
 
-class TodoAdapter {
+import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TodoAdapter(
+    private val todos: MutableList<Todo>
+) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+
+    class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        return TodoViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_todo,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        val curTodo = todos[position]
+    }
+
+    override fun getItemCount(): Int {
+        return todos.size
+    }
 }
